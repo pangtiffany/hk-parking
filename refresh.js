afd17eed27a23629b3b8c51c78740358c52ee36a
pangtiffany <pangtiffanyyyyyyyy@gmail.com>
2026-04-02 12:27:00 +0800

    Update refresh.js

--- refresh.js
+++ refresh.js
@@ -1,1 +1,22 @@
+async function loadData() {
+  allCarparks = await fetchCarparks(currentLang);
 
+  renderList(allCarparks);
+  updateMap(allCarparks);
+  populateDistrictFilter(allCarparks);
+}
+
+document.getElementById("nearMeBtn").addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(pos => {
+    map.setView(
+      [pos.coords.latitude, pos.coords.longitude],
+      15
+    );
+  });
+});
+
+// Initial load
+loadData();
+
+// Auto refresh every 60 seconds
+setInterval(loadData, 60000);
